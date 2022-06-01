d70f35215fe360f283903bb873b5b19f6644416d
laura zambrano <laura.zambrano@uao.edu.co>
2022-06-01 12:21:22 +0200

    changed hello messages

--- src/com/ironhack/helloword/main/Main.java
+++ src/com/ironhack/helloword/main/Main.java
@@ -3,5 +3,6 @@ package com.ironhack.helloword.main;
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello world :)");
+        System.out.println("Goodbye world :)");
     }
 }
