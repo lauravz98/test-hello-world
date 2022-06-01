7ae8ba2f2c99b5d7988ba9598aa7f149ebe73eae
laura zambrano <laura.zambrano@uao.edu.co>
2022-06-01 12:33:53 +0200

    change mensaje

--- src/com/ironhack/helloword/main/Main.java
+++ src/com/ironhack/helloword/main/Main.java
@@ -2,7 +2,8 @@ package com.ironhack.helloword.main;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world :)");
+        System.out.println("cambuio aaaa" +
+                " world :)");
         System.out.println("Goodbye world :)");
 
         System.out.println("Esto fue creado en la rama new-feature");
